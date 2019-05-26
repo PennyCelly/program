91ac855906ed910e2e384e7a2101cae71f37f3ea
[PennyCelly] <duwei_n@163.com>
2019-05-26 22:23:26 +0800

    数据结构

---
+++ STUDY/数据结构/列表/列表作堆栈使用.py
@@ -0,0 +1,11 @@
+﻿"""
+堆栈：后进先出
+列表用 append() 方法可以把一个元素添加到堆栈顶。用不指定索引的 pop() 方法可以把一个元素从堆栈顶释放出来
+"""
+stack = ['q', 'w', 3, 4, 7, 5, 9]
+stack.append(8)
+for i in stack:
+    stack.pop()                         # 到7的时候，7已经被pop掉了，循环截止
+    print(stack)
+    print(i)
+
